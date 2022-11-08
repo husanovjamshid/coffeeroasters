febff00d88679494cff3bd47990e9a6b0734f40e
husanovjamshid <husanovjamshid11@gmail.com>
2022-11-08 22:57:05 +0500

    finished project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
 let cardBtn = document.querySelector('.coffee__choose')
+let cardBtn2 = document.querySelector('.coffee__choose-two')
+let cardBtn3 = document.querySelector('.coffee__choose-three')
+let cardBtn4 = document.querySelector('.coffee__choose-four')
+let cardBtn5 = document.querySelector('.coffee__choose-five')
 let openBtn = document.querySelector('.open')
 let closeBtn = document.querySelector('.coffee__choose-title')
+let closeBtn2 = document.querySelector('.coffee__choose-title-two')
+let closeBtn3 = document.querySelector('.coffee__choose-title-three')
+let closeBtn4 = document.querySelector('.coffee__choose-title-four')
+let closeBtn5 = document.querySelector('.coffee__choose-title-five')
 
 closeBtn.addEventListener('click', function(){
     cardBtn.classList.toggle('open')
+})
+
+closeBtn2.addEventListener('click', function(){
+    cardBtn2.classList.toggle('open')
+})
+
+closeBtn3.addEventListener('click', function(){
+    cardBtn3.classList.toggle('open')
+})
+
+closeBtn4.addEventListener('click', function(){
+    cardBtn4.classList.toggle('open')
+})
+
+closeBtn5.addEventListener('click', function(){
+    cardBtn5.classList.toggle('open')
 })
